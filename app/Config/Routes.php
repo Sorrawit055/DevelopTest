a2b848b25fe9dcb3dab9681045e3d01a43b6c1ab
Sorrawit Eaknagonpan <614259055@webmail.npru.ac.th>
2022-06-28 21:23:07 +0700

    Update

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -36,7 +36,8 @@ $routes->set404Override();
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'LoginController::index');
-
+$routes->get('/signin', 'loginController::index');
+$routes->post('/loginAuth', 'loginController::loginAuth');
 /*
  * --------------------------------------------------------------------
  * Additional Routing
